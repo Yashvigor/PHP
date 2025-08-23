1b01ca001b3eba5c6e3c2e5fac78b129d2231d47
Yashvigor <yashvigor.ce@gmail.com>
2025-08-23 12:28:56 +0530

    Add files via upload

--- Practice/US_trip/index.php
+++ Practice/US_trip/index.php
@@ -1,1 +1,14 @@
+<?php
+$servername = "localhost";
+$username   = "root";   // default XAMPP user
+$password   = "";       // default password is empty
+$database   = "us_trip";  // ✅ use 'us_trip' instead of 'trip'
 
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
